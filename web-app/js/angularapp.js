d0572bb8d01114d110213c94ee08ac9cb970f256
Trond Valen <trondvalen@gmail.com>
2014-07-06 13:17:09 +0200

    Simple CRUD in place. Changed layout

--- web-app/js/angularapp.js
+++ web-app/js/angularapp.js
@@ -7,7 +7,7 @@ angular.module('angularapp', ['ngRoute', 'firebase'])
       controller:'ListCtrl',
       templateUrl:'list.html'
     })
-    .when('/edit/:projectId', {
+    .when('/edit/:recipeId', {
       controller:'EditCtrl',
       templateUrl:'detail.html'
     })
@@ -19,26 +19,51 @@ angular.module('angularapp', ['ngRoute', 'firebase'])
       redirectTo:'/'
     });
 })
+
+.value('fbUrl', 'https://blinding-fire-2931.firebaseio.com/recipes')
  
-.controller('ListCtrl', function($scope, $firebase) {
-  var recipesRef = new Firebase("https://blinding-fire-2931.firebaseio.com/recipes");
+.controller('ListCtrl', function($scope, $firebase, fbUrl) {
+  var recipesRef = new Firebase(fbUrl);
   $scope.recipes = $firebase(recipesRef);
 })
 
-.controller('CreateCtrl', function($scope, $location, $timeout, $firebase) {
+.controller('CreateCtrl', function($scope, $location, $firebase, fbUrl) {
   console.log("CreateCtrl");
 
-  var recipesRef = new Firebase("https://blinding-fire-2931.firebaseio.com/recipes");
+  var recipesRef = new Firebase(fbUrl);
   $scope.recipes = $firebase(recipesRef);
 
   $scope.save = function() {
     console.log("New");
-    $scope.recipes.$add($scope.newRecipe);
+    $scope.recipes.$add($scope.recipe);
     
-    $scope.newRecipe = {};
+    $scope.recipe = {};
     
     $location.path('/');
   }
+})
+
+.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbUrl) {
+  $scope.recipe = $firebase(new Firebase(fbUrl + '/' + $routeParams.recipeId));
+
+  $scope.save = function() {
+    console.log($scope.recipe.name)
+  
+    $scope.recipe.$transaction(function(currentRecipe) {
+        currentRecipe.name = $scope.recipe.name
+        currentRecipe.tags = $scope.recipe.tags
+        return currentRecipe;
+    }).then(function(snapshot) {
+        if (!snapshot) {
+          console.log("Error")
+        } else {
+          $location.path('/')
+        }
+    }, function(err) {
+        console.log(err)
+    });
+  }
+
 })
   
 
