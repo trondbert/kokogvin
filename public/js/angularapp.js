67aaa91fd76eaa9cf57fd30baed2d5765e05173d
Trond Valen <trondvalen@gmail.com>
2014-07-13 22:52:24 +0200

    Mostly file upload, styling, more attributes

--- public/js/angularapp.js
+++ public/js/angularapp.js
@@ -1,7 +1,7 @@
 
-angular.module('angularapp', ['ngRoute', 'firebase'])
+app = angular.module('angularapp', ['ngRoute', 'firebase'])
   
-.config(function($routeProvider) {
+app.config(function($routeProvider) {
   $routeProvider
     .when('/', {
       controller:'ListCtrl',
@@ -20,14 +20,14 @@ angular.module('angularapp', ['ngRoute', 'firebase'])
     });
 })
 
-.value('fbUrl', 'https://blinding-fire-2931.firebaseio.com/recipes')
+app.value('fbUrl', 'https://blinding-fire-2931.firebaseio.com/recipes')
  
-.controller('ListCtrl', function($scope, $firebase, fbUrl) {
+app.controller('ListCtrl', function($scope, $firebase, fbUrl) {
   var recipesRef = new Firebase(fbUrl);
   $scope.recipes = $firebase(recipesRef);
 })
 
-.controller('CreateCtrl', function($scope, $location, $firebase, fbUrl) {
+app.controller('CreateCtrl', function($scope, $location, $firebase, fbUrl) {
   console.log("CreateCtrl");
 
   var recipesRef = new Firebase(fbUrl);
@@ -43,15 +43,18 @@ angular.module('angularapp', ['ngRoute', 'firebase'])
   }
 })
 
-.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbUrl) {
+app.controller('EditCtrl', function($scope, $location, $routeParams, $firebase, fbUrl) {
   $scope.recipe = $firebase(new Firebase(fbUrl + '/' + $routeParams.recipeId));
 
   $scope.save = function() {
     console.log($scope.recipe.name)
   
     $scope.recipe.$transaction(function(currentRecipe) {
-        currentRecipe.name = $scope.recipe.name
-        currentRecipe.tags = $scope.recipe.tags
+        currentRecipe.name = $scope.recipe.name;
+        currentRecipe.tags = $scope.recipe.tags;
+        currentRecipe.image = $scope.recipe.image;
+        currentRecipe.instructions = $scope.recipe.instructions;
+        currentRecipe.ingredients = $scope.recipe.ingredients;
         return currentRecipe;
     }).then(function(snapshot) {
         if (!snapshot) {
@@ -64,6 +67,31 @@ angular.module('angularapp', ['ngRoute', 'firebase'])
     });
   }
 
+  $scope.changeImage = function() {
+    var file = $("#recipeImage")[0].files[0],
+    reader = new FileReader();
+    
+    reader.onloadend = function(e){
+      var imageBase64 = e.target.result;
+      console.log(imageBase64);
+      $scope.recipe.image = imageBase64;
+      $scope.$apply();
+    }
+
+    reader.readAsDataURL(file);
+  }
 })
-  
+
+app.directive('customOnChange', function() {
+  'use strict';
+
+  return {
+    restrict: "A",
+    link: function (scope, element, attrs) {
+      var onChangeFunc = element.scope()[attrs.customOnChange];
+      element.bind('change', onChangeFunc);
+    }
+  };
+});
+
 
